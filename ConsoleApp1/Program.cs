eacaa0467cffe268fdf542369d44b5f297c735bb
Yippee.max <GabeN3well@gmail.com>
2024-09-13 14:11:56 +0300

    bish bosh having a wosh

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Aye, this be a message for the weary sailors out there
+
+// programm kusib nime sisestada
+// programm siis utleb tervist
+
+Console.WriteLine("Enter your name");
+// string on sone
+string userName = Console.ReadLine();
+Console.WriteLine("Hello," + userName + "!");
+// string interpolation
+
+Console.WriteLine($"Hello, {userName} !");
